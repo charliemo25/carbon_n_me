d3b1237afa4f6dbb67c916649f0fd4fa2e0a37d7
Benoit70 <benoit.d@codeur.online>
2017-12-22 11:22:03 +0100

    page voiture

--- models/getVoiture.php
+++ models/getVoiture.php
@@ -7,4 +7,25 @@ function getVoiture($pdo){
 	return $result;
 }
 
+function getEnergie($pdo){
+	$req = $pdo->prepare('select etiquette_energie.label from etiquette_energie inner join voiture on voiture.id_etiquette_energie=etiquette_energie.id_etiquette_energie where voiture.id_voiture=5');
+	$req->execute();
+	$result = $req->fetchAll();
+	return $result;
+}
+
+function getBonus($pdo){
+	$req = $pdo->prepare('select bonus.montant FROM bonus INNER JOIN voiture on bonus.id_bonus=voiture.id_bonus WHERE voiture.id_voiture=5');
+	$req->execute();
+	$result = $req->fetchAll();
+	return $result;
+}
+
+function getMalus($pdo){
+	$req = $pdo->prepare('select malus.montant from malus inner join voiture on malus.id_malus=voiture.id_malus where voiture.id_voiture=5');
+	$req->execute();
+	$result = $req->fetchAll();
+	return $result; 
+}
+
 ?>
